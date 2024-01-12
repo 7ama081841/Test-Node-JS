754aae6536cdc98d0393e13d02b3471d5570d0c8
mohamed123 <chaabenim318@gmail.com>
2024-01-12 21:48:38 +0100

    update file order.js ( routs )

--- routes/order.js
+++ routes/order.js
@@ -4,7 +4,7 @@ const admin = require("../middlewares/admin");
 const auth = require("../middlewares/auth");
 
 router.post("/order", auth, orderControllers.addOrder);
-router.get("/orders", [auth, admin], orderControllers.getOrderList);
-router.get("/order/:orderId", [auth, admin], orderControllers.getOneOrder);
+router.get("/orders", auth, orderControllers.getOrderList);
+router.get("/order/:orderId", auth, orderControllers.getOneOrder);
 
 module.exports = router;
